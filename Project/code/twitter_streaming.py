2c06ad7cc626577e2aef4664ba8462d260c7d17a
Yuhai Li <liyuhai6666@gmail.com>
2017-11-08 13:01:58 -0800

    update crawler

--- Project/code/twitter_streaming.py
+++ Project/code/twitter_streaming.py
@@ -26,7 +26,7 @@ out_file = open("stream_output.json", "w")
 # Here we set it to stop after getting 1000 tweets. 
 # You don't have to set it to stop, but can continue running 
 # the Twitter API to collect data for days or even longer. 
-tweet_count = 10
+tweet_count = 10000
 for tweet in iterator:
     tweet_count -= 1
     # Twitter Python Tool wraps the data returned by Twitter 
